be2806cc75ee0b3407e9da0992ad29cd000bbfde
NachiMeguro <nachikinokuni@gmail.com>
2024-08-08 16:06:48 +0800

    Challneges edded

---
+++ Day2/challenges.js
@@ -0,0 +1,34 @@
+/* 
+### Day 2 JavaScript Challenges
+
+#### Exercise 1: Variables and Constants
+1. Declare a variable to store your age and print it to the console.
+2. Declare a constant to store the value of pi (3.14159) and print it to the console.
+
+#### Exercise 2: Assigning Values
+3. Declare two variables, `x` and `y`, and assign them values of 5 and 10 respectively. Print their values to the console.
+
+#### Exercise 3: Swapping Values
+4. Write a program that swaps the values of two variables without using a third variable. For example, if `a = 1` and `b = 2`, after swapping, `a` should be 2 and `b` should be 1.
+
+#### Exercise 4: Simple Arithmetic
+5. Declare three variables: `a`, `b`, and `c`. Assign them values of 3, 7, and 2 respectively. Perform the following operations and print the results:
+   - Addition of `a` and `b`
+   - Subtraction of `b` and `c`
+   - Multiplication of `a` and `c`
+   - Division of `b` by `a`
+   - Remainder of `b` divided by `c`
+
+#### Exercise 5: Increment and Decrement
+6. Declare a variable `num` with an initial value of 10. Increment the variable by 1 and print the result. Then decrement the variable by 1 and print the result.
+
+#### Exercise 6: Compound Assignment
+7. Declare a variable `total` with an initial value of 50. Use compound assignment operators to:
+   - Add 20 to `total`
+   - Subtract 10 from `total`
+   - Multiply `total` by 2
+   - Divide `total` by 5
+   - Print the result after each operation
+
+Good luck with your exercises! Once you're ready, we can review the solutions together.
+*/
